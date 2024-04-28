4d2063fe61793b7e2e160a0b1d089598157bbd19
Helga-Vi <katarina.jorgensen@gmail.com>
2024-04-28 18:29:22 +0200

    setup for testing CRUD on localhost

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/users');
 
 //const {s3} = require('../AWSconfig.js');
 //const {docClient} = require('../AWSconfig.js');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
+  try { 
   let list = await users.list();
   res.send(list);
+} catch (error) {
+  res.status(500).send({ error: 'Failed to list users' });
+}
 });
 
 router.get('/:key', async function(req, res, next) {
+  try {
   let item = await users.get(req.params.key);
-  res.send(item);
+  if (item) {
+    res.send(item);
+  } else {
+    res.status(404).send({ error: 'User not found' });
+  }
+} catch (error) {
+  res.status(500).send({ error: 'Failed to get user' });
+}
 });
 
 router.post('/', async function(req, res, next) {
+  try {
   const {email, firstName, lastName, age} = req.body;
   await users.set(email, {
     firstName: firstName,
     secondName: lastName,
     age: age
-  })
-  res.end();
+  });
+  res.status(201).end();
+ } catch (error) {
+    res.status(500).send({ error: 'Failed to create user' });
+ }
 });
 
 router.put('/', async function(req, res, next) {
+  try {
   const {email, firstName, lastName, age} = req.body;
   await users.set(email, {
     firstName: firstName,
     secondName: lastName,
     age: age
-  })
+  });
   res.end();
+} catch (error) {
+  res.status(500).send({ error: 'Failed to update user' });
+}
 });
 
 router.delete('/:key', async function(req, res, next) {
+try {
   await users.delete(req.params.key);
   res.end();
+} catch (error) {
+  res.status(500).send({ error: 'Failed to delete user' });
+}
 });
 module.exports = router;
