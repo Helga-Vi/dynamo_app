130640b0b496d89df66baf37fd1bdc90a471b5ef
Helga-Vi <katarina.jorgensen@gmail.com>
2024-04-29 01:29:37 +0200

    deletion

--- AWSconfig.js
+++ AWSconfig.js
@@ -2,18 +2,8 @@
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient} = require("@aws-sdk/lib-dynamodb");
-//require('dotenv').config();
+
 console.log("This is aws-config file responding");
-
-//configure the AWS SDK 
-
-//const client = new DynamoDBClient({
-    //region: process.env.AWS_REGION,
-    //credentials: {
-    //accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    //secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    //}
-//});
 
 const client = new DynamoDBClient({ region: "eu-north-1" });
 const docClient = DynamoDBDocumentClient.from(client);
