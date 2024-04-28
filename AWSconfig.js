69057835b647d39ac9426ffd766399403e3294cc
Helga-Vi <katarina.jorgensen@gmail.com>
2024-04-28 22:52:57 +0200

    updating config

--- AWSconfig.js
+++ AWSconfig.js
@@ -1,18 +1,21 @@
 //AWS CONFIG FILE
 
-const AWS=require('aws-sdk');
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 require('dotenv').config();
 console.log("This is aws-config file responding");
 
 //configure the AWS SDK 
 
-AWS.config.update({
+const client = new DynamoDBClient({
     region: process.env.AWS_REGION,
+    credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
 });
 
 //const s3=new AWS.S3();
-const docClient = newAWS.DynamoDB.DocumentClient();
+const docClient = DynamoDBDocumentClient.from(client);
 
-module.exports= docClient;
+module.exports= {docClient};
